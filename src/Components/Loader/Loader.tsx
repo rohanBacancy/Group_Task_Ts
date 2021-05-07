7495eef164bc34f8cd8f95cb19407de0a2a956d7
rohan-empire-k <rohan.bhatt@bacancy.com>
2021-05-07 15:52:46 +0530

    Posts Page done

---
+++ src/Components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import './Loader.css'
+import React from 'react'
+
+const Loader:React.FC = () => {
+    return (
+        <div className="spinner"></div>
+    )
+}
+
+export default Loader
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,33 @@
-import React,{FC} from 'react'
+import axios from 'axios'
+import React,{FC, useEffect, useState} from 'react'
+import { Container, Row } from 'reactstrap';
+import Loader from '../Components/Loader/Loader';
+import PostCard from '../Components/PostCard';
+import {Post} from '../Interfaces/interfaces'
 
 const Dashboard:FC = () => {
+
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [forceRenderPosts,setForceRenderPosts] = useState<number>(0);
+
+    useEffect(() => {
+        console.log("Posts ReFetched")
+        axios.get("https://6094e51894009e00176b5f56.mockapi.io/posts")
+        .then(res => {setPosts(res.data); console.log(res.data)})
+        .catch(err => console.log(err))
+    }, [forceRenderPosts])
+
     return (
-        <div>
-            dashboard
-        </div>
+        <Container>
+            <Row>
+                {
+                    posts 
+                    ? posts.map(post => <PostCard key={post.id} post={post} setForceRenderPosts={setForceRenderPosts}/>)
+                    : <Loader/>
+                }
+                <div>Put Pagination Here</div>
+            </Row>
+        </Container>
     )
 }
 
